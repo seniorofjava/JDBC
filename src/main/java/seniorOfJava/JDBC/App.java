eb4ad3fc6638a7b34ecfaf46cd0b0f03f096b029
Volnenko Alexey <am888.ua@gmail.com>
2016-05-29 09:25:57 +0300

    second commit

--- src/main/java/seniorOfJava/JDBC/App.java
+++ src/main/java/seniorOfJava/JDBC/App.java
@@ -4,29 +4,27 @@ import java.sql.*;
 
 public class App 
 {
-	public static void main (String[] args)
-	{
-	Connection conn = null;
-	try
-	{
-	String userName = "root";
-	String password = "11111";
-	String url = "jdbc:mysql://localhost/test"; 
-	Class.forName ("com.mysql.jdbc.Driver").newInstance ();
+	static final String userName = "root";
+	static final String password = "11111";
+	static final String url = "jdbc:mysql://localhost/test";
+	
+	static Connection conn = null;
+	
+	public static void main (String[] args){
+	
+	try{
+	 
+	//Class.forName ("com.mysql.jdbc.Driver").newInstance ();
 	conn = DriverManager.getConnection (url, userName, password);
 	System.out.println ("Database connection established");
 	}
-	catch (Exception e)
-	{
+	catch (Exception e){
 	System.err.println ("Cannot connect to database server");
 	e.printStackTrace();
 	}
-	finally
-	{
-	if (conn != null)
-	{
-	try
-	{
+	finally{
+		if (conn != null){
+	try{
 	conn.close ();
 	System.out.println ("Database connection terminated"); 
 	}
